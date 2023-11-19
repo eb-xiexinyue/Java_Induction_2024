b5fe12d8a5df1e97f11d44cf45ed7e37822cbe2e
Owner <Owner@LAPTOP-O87PRPJO>
2023-11-19 23:50:16 +0900

    2023.11.19

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -9,18 +9,25 @@ public class JavaHomeWork01 {
 		// 質問０１
 		// int i = 10;
 		// int n = i%5;
+		int i = 10;
+		int n = i%5;
 
 		// N = XXXXX
+		System.out.println(n);
+		}
+}
 
 		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
-		// 回答XXXXXXX
+		// 回答: a > 10の式を評価する,true の場合、11を返す,falseの場合0を返す
 
-		// 質問３︓変数値を⽐較する演算⼦は = or == ?
+		// 質問３︓変数値を⽐較する演算⼦は = or == ？　答え：＝＝
 
 
 		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		//true または false
 
 
-	}
 
-}
+
+
+
