17e657d6af72852cd852c04055c568eb2795abb6
kertiticaca <kertiticaca@localhost>
2023-11-20 22:07:35 +0900

    20231120

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,18 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		int a = 10;
+		System.out.println("a");
+		
+		
+       
+    		   
 
 	}
 }
+// 質問１　１行
+// 質問２　equals
+// 質問３　String.valueOf("1.0F")
+// 質問４　３　/  12.0
+// 質問５　a
