4768e9c5cc65e65faf3f90ebe1eb5faf913126ff
zyx <zyx@localhost>
2023-11-20 22:26:33 +0900

    20231120

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,26 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
+		// 質問１　System.out.println(“this is line1\tthis line2“); は何行出力しますか?
+		System.out.println("this is line1\tthis is line2");
+		// 回答：２行
+		
+		// 質問２　Stringを比較方法は?
+		// 回答：equals
+		
+		// 質問３　Float値⇒String文字列変更する場合、どうなメソッドを利用しますか
+		// 回答：String. valueOf("1.0F")
+		
+		// 質問４　
+		/* 回答
+		 * 　　　1.  3.0
+		 *      2.  "3.0"
+		 */     
+		
+		// 質問５
+		int a = 10;
+		System.out.println("a");
+		// 回答: a
 
 	}
 }
