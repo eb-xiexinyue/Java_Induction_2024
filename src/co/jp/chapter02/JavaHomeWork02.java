11d931d30b7ea133839a6a69abb9d4eaf9bbd045
luosan <luosan@localhost>
2023-11-20 21:26:01 +0900

    2023/11/20

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,22 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
-
+		//質問1の回答：1. 1行
+		System.out.println("this is line1\tthis line2");
+		
+		//質問2の回答：2.equals
+		
+		//質問3の回答：2. 1.0F + ““
+		
+		//質問4の回答：1: 3.0      2: 1.02.0
+		System.out.println(1.0 + 2.0);
+		System.out.println(1.0 + "2.0");
+		
+		//質問５の回答: a
+		int a = 10;
+		System.out.println("a");
+		
+	
+		
 	}
 }
