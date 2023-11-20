bac91e3857665bd0f03a7fbdc33170112e266a1e
chl07 <chl07@192.168.1.6>
2023-11-20 17:16:20 +0900

    2023.11/20 cui_huilian

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -6,19 +6,26 @@ public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
 
-		// 質問０１
+		// 質問１
 		// int i = 10;
 		// int n = i%5;
+		// 回答：n = 0
+		int i = 10;
+		int n = i%5;
+		System.out.println("変数nの値は" + n);
 
-		// N = XXXXX
 
 		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
-		// 回答XXXXXXX
+		// 回答：「?:」条件演算子であるため、上記の式を解釈すると、
+		// a > 10の場合、変数aに11を返して、そうでない場合は0を返す。
+		
 
 		// 質問３︓変数値を⽐較する演算⼦は = or == ?
+		// 回答：==（関係演算子の一つである）
 
 
 		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		// 回答：true または　false
 
 
 	}
