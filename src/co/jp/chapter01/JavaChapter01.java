377aab0bb706ed1ef126e68c1d0b23cb0361e484
xie <xie@LAPTOP-N0ONNUGE>
2023-11-20 17:11:19 +0900

    20231120 commit

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -1,11 +1,70 @@
 package co.jp.chapter01;
 
 public class JavaChapter01 {
+	
+//	final int Number3 = 99;
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-		System.out.println("hello world");
-
+		
+//		char character;
+//		character = 'T';
+//		
+//		String str = "test";
+		
+		//final int Number3 = 99;
+		
+//		float longInt = 99.9f;
+//		
+//		double doubleInt = 99.9f;
+//		
+//		boolean b = true;
+		
+		//Number3 = 20;
+		
+		int a = 3;
+		
+		//a++ 
+		//①　a=a+1
+		
+		int b = 3;
+				
+//		int parseFromFloat = (int)longInt;
+		 
+//		System.out.println("char type is "+character);
+//		System.out.println("演算子　a+b="+(a+b));
+//		System.out.println("演算子　a-b="+(a-b));
+//		System.out.println("演算子　a*b="+(a*b));
+//		System.out.println("演算子　a/b="+(a/b));
+//		System.out.println("演算子　a%b="+(a%b));
+		
+//		System.out.println("演算子　++a="+(++a));
+//		System.out.println("演算子　a="+a);
+//		
+//		System.out.println("演算子　--b="+(--b));
+//		System.out.println("演算子　b="+b);
+		
+		System.out.println("演算子　a!=b "+(a!=b));
+		System.out.println("演算子　a>b "+(a==b));
+		System.out.println("演算子　a>=b "+(a==b));
+		System.out.println("演算子　a<b "+(a<b));
+		System.out.println("演算子　a<=b "+(a<=b));
+		
+		System.out.println("演算子　a>b "+(a>b));
+		System.out.println("演算子　!a>b "+!(a>b));
+		
+		System.out.println("演算子　|| "+((2>3)||(2>1)));
+		System.out.println("演算子　&& "+((2>3)&&(2>1)));
+//		System.out.println("float type is "+longInt);
+//		System.out.println("boolean type is "+b);
+//		
+//		System.out.println("double type is "+doubleInt);
+//		
+//		System.out.println("parse from flaot to int :"+parseFromFloat);
+		
+		System.out.println("条件論理演算⼦"+(2>3?"真":"偽"));
 	}
 }
+
+
+
