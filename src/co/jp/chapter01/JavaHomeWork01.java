352f74e5c2e3ffd4403c67786267344033e6d07e
zoupeihua <zoupeihua@localhost>
2023-11-20 22:06:23 +0900

    20231120 commit

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -11,8 +11,8 @@ public class JavaHomeWork01 {
 		// int n = i%5;
 
 		// N = 0
-		変数iに整数値10が代入されている。そしてnにはiを5で割った余りが代入される
-		10を5で割ると余りは0。　だからnには0が代入される。
+		//変数iに整数値10が代入されている。そしてnにはiを5で割った余りが代入される
+		//10を5で割ると余りは0。　だからnには0が代入される。
 
 		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
 		// 回答:この式は「a が10より大きいかどうかを判定し、真であれば11を代入し、偽であれば0を代入する」という動作を表している。
@@ -29,8 +29,8 @@ public class JavaHomeWork01 {
 
 
 		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は 「true」「false」
-		 boolean is true=true;
-                 boolean is false=false;
+		 //boolean is true=true;
+           //      boolean is false=false;
 
 	}
 
