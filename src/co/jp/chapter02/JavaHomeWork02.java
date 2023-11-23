e35ea59289b74897d699e66d8a3b2a0ab3ad576e
lee <lee@LAPTOP-10QMED6H>
2023-11-24 02:13:45 +0900

    20231124コミット

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,33 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
+		 System.out.println("this is line1\tthis line2");
+		 System.out.println(1.0+2.0 );
+		 System.out.println(1.0+"2.0");
+		 
+		 
+		/*
+		質問１：System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖ 
+		回答：1. 1行
+		
+		質問２︓Stringを⽐較⽅法は︖
+		回答：2. equals
+		
+		質問３︓Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
+		回答：2. 1.0F + ““
+		
+		質問４︓以下式の値を記載してください。
+		1.0 + 2.0の実行の結果は3.0
+		1.0 + “2.0“の結果は1.02.0
+		
+		質問５︓以下ステートメントの出⼒を記載してください。
+		回答：出力の結果は a です。
+		*/
+		 int a = 10;
+		 System.out.println("a");
+		 
+
+		 
 
 	}
 }
