28da7fd96f692c2d605703465ab3b5c9c62be97a
zoupeihua <zoupeihua@localhost>
2023-11-24 11:45:47 +0900

    2023 1124

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -4,5 +4,13 @@ public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
 
-	}
-}
+		
+		
+//質問1：1. 1行
+//質問2：2. equals
+//質問3　1. String.valueOf(“1.0F“)
+//質問4:以下式の値を記載してください。
+//1. 1.0 + 2.0＝3 　　　2. 1.0 + “2.0“="1.02.0"
+//質問5：”a”
+		
+		
