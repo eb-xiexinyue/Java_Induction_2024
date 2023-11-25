829da5bc54faafcad6b92ea0bf872a40e8ae8b24
81803 <81803@MyComputer.elecom>
2023-11-25 19:45:35 +0900

    2023 11 25

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -4,5 +4,21 @@ public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
 
+		
+		//質問１︓ System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖
+		//答えは1行
+		
+		//質問２︓Stringを⽐較⽅法は︖
+		//答えはequals
+		
+		//質問３︓Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
+		//答えはString.valueOf(“1.0F“)
+		
+		//質問４︓以下式の値を記載してください。
+		// 3.0
+		//12.0
+		
+		//質問５︓以下ステートメントの出⼒を記載してください。
+		//答えはa
 	}
 }
