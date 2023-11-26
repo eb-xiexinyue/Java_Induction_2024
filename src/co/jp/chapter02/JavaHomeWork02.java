0ab5edc7fd28d9754dd96ac147b415fb1234cfd2
ZHAO　DANYING <88527845+11286866@users.noreply.github.com>
2023-11-26 21:42:17 +0800

    1126コミット

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -7,19 +7,20 @@ public class JavaHomeWork02 {
 		
 		//質問１：１行
 		
-        //質問2：2equals
+               //質問2：2equals
 		
 		//質問3：1. String.valueOf(“1.0F“)
 		
-		//質問4： 1.0 + 2.0得出的值为“3.0”，1.0+“2.0”得出的值为“1.02.0”。
+		//質問4： 1.0 + 2.0得出的值为“3.0”。
 		double m=1.0;
 		double n=2.0;
+		
 		System.out.println(1.0+2.0);
 		
+		//1.0+“2.0”得出的值为“1.02.0”。
 		System.out.println(1.0+"2.0");
 		
 		//質問5：输出"a"
-		
 
 		 int a =10;
 		 System.out.println("\""+"a"+"\"");
