09309ee6f3db1b41d8879a5b5d503e94e0397ccd
zyx <zyx@localhost>
2023-11-28 00:02:04 +0900

    20231127

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -4,7 +4,48 @@ public class JavaHomeWork03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+//　質問１
+ int a = 56;
+ System. out. println("成績ランキング判断。入力値=" + a);
+ if(a>=90) {
+	 System.out.println("A");
+ }else if(a>=80) {
+	 System.out.println("B");
+ }else if(a>=70) {
+	 System.out.println("C");
+ }else if(a>=60) {
+	 System.out.println("D");
+ }else {
+	 System.out.println("E");
+	 
+	
+
+// 質問２
+	 int x = 3340;
+	 int x500 = x / 500;
+	 System.out.println("500円の枚数 = "+x500);
+	 
+	 x = x % 500;
+	 int x100 = x / 100;
+	 System.out.println("100円の枚数 = "+x100);
+	 
+	 x = x % 100;
+	 int x10 = x / 10;
+	 System.out.println("10円の枚数 = "+x10);
+	 
+	 x = x % 10;
+	 int x5 = x / 5;
+	 System.out.println("5円の枚数 = "+x5);
+	 
+	 x = x % 5;
+	 int x1 = x / 1;
+	 System.out.println("1円の枚数 = "+x1);
+	 
+	 // 500円6枚、100円3枚、10円4枚、5円0枚、1円0枚
+
+	}}}
+
+	
 
 
-	}
-}
+	
