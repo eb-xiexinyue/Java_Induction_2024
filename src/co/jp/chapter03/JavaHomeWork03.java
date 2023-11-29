4c0935addecca81a52d64bf101f126150f23d68b
zoupeihua <zoupeihua@localhost>
2023-11-29 21:54:07 +0900

    20231129

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -1,10 +1,67 @@
 package co.jp.chapter03;
 
-public class JavaHomeWork03 {
+//public class JavaHomeWork03 {
 
-	public static void main(String[] args) {
+//	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
 
-	}
-}
+//	}
+//	{
+
+// 質問1		
+// public static String calculateRank(int score) {
+//       if (score >=90) {
+//    	   return "A";
+//       } else if (score >= 80) {
+//    	   return "B";
+//       } else if (score >= 70) {
+//       	   return "C";
+//       } else if (score >= 60) {
+//    	   return "D";
+//    	 }else {
+//    		 return "E";
+    				
+//    	 }
+//     }
+//   }     
+
+
+//質問2
+//import java.util.Scanner;
+
+//public class CoinCount {
+//    public static void main(String[] args) {
+//       
+//		Scanner scanner = new Scanner(System.in);
+//        System.out.print("3340円");
+//        int amount = scanner.nextInt();
+
+        
+//        int[] coinCounts = calculateCoinCount(amount);
+
+        
+//        System.out.println("5枚");
+//        System.out.println("500円: " + coinCounts[0] + " 枚");
+//        System.out.println("100円: " + coinCounts[1] + " 枚");
+//        System.out.println("10円: " + coinCounts[2] + " 枚");
+//        System.out.println("5円: " + coinCounts[3] + " 枚");
+//        System.out.println("1円: " + coinCounts[4] + " 枚");
+
+        
+//        scanner.close();
+
+
+    
+//    public static int[] calculateCoinCount(int amount) {
+//        int[] coinValues = {500, 100, 10, 5, 1};
+//        int[] coinCounts = new int[5];
+
+//        for (int i = 0; i < coinValues.length; i++) {
+//            coinCounts[i] = amount / coinValues[i];
+//            amount %= coinValues[i];
+//        }
+
+//        return coinCounts;
+//    }
+// }
