f80d7f0cb0ed0beb05e3865acec658bb394c5c46
李展雲 <239398873@qq.com>
2023-12-03 19:53:39 +0900

    2023.12.3homework03.java

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -4,7 +4,62 @@ public class JavaHomeWork03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//2023.11.27上课课堂练习
+//		 for(int i=0;i<11;i++) {
+//			for(int j=0;j<11;j++) {
+//				if(i==j||i+j==10||i==5||j==5 ) {
+//					System.out.print(i+""+j+"	");
+//				}else {
+//					System.out.print("	");
+//				}
+//			}
+//			System.out.println();
+//		}
+		// 質問１︓成績ランキング判断を作成する「90以上︓A, 80-90︓B, 70-80︓C, 60-70︓
+		// D, 60以下︓E」
+//		 int a = 56;
+//		 System.out.println("成績ランキング判断。入力値=" + a);  
+//		 if(a >= 90) { 
+//		 System.out.println("A"); 
+//		 } else if(a >= 80 && a < 90) { 
+//		 System.out.println("B"); 
+//		 } else if(a >= 70 && a < 80) { 
+//		 System.out.println("C"); 
+//		 } else if(a >= 60 && a < 70) { 
+//		 System.out.println("D"); 
+//		 } else { 
+//		 System.out.println("E"); 
+//		 }
+		// 質問２︓⽇本のコインを１円、５円、１０円、１００円、５００円があります。３３４０ 
+		 //円のコイン数をもとめください。
 
+		int amount = 3340;
+		int count500 = amount / 500;//%= 是一个复合赋值运算符，表示取余并赋值.
+		amount %= 500;//将 amount 除以 500 得到余数，并将余数赋值给 amount,
+                      //amount 的值就更新为剩下的金额，即 340 元。
+		int count100 = amount / 100;
+		amount %= 100;
 
+		int count10 = amount / 10;
+		amount %= 10;
+
+		int count5 = amount / 5;
+		amount %= 5;
+
+		int count1 = amount;
+
+		System.out.println("500円: " + count500 + "枚");
+		System.out.println("100円: " + count100 + "枚");
+		System.out.println("10円: " + count10 + "枚");
+		System.out.println("5円: " + count5 + "枚");
+		System.out.println("1円: " + count1 + "枚");
+
+		
+		
+		 
+	
+		
+		
 	}
+
 }
