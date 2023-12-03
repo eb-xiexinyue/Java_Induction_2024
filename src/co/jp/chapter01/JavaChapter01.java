39184797f5b23767822f3b1cf7a88305cf515f01
Owner <Owner@LAPTOP-O87PRPJO>
2023-12-03 16:05:47 +0900

    12.03

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -3,9 +3,22 @@ package co.jp.chapter01;
 public class JavaChapter01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		int n = 11; // 菱形的边长
 
-		System.out.println("hello world");
+		for (int i = 0; i < n; i++) {
+		    for (int j = 0; j < n; j++) {
+		        if (i + j >= n / 2 && i <= j && i + j <= 3 * n / 2) {
+		            if (i == 0 || i == n - 1 || j == 0 || j == n - 1 || i + j == n / 2 || i + j == 3 * n / 2) {
+		                System.out.print(i + "" + j + "  ");
+		            } else {
+		                System.out.print("   ");
+		            }
+		        } else {
+		            System.out.print("   ");
+		        }
+		    }
+		    System.out.println();
+		}
 
 	}
 }
--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -5,6 +5,32 @@ public class JavaHomeWork03 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+	//質問１
+		int a = 56;
+		System.out.println("成績ランキング判断。入力値=" + a);
 
+		if (a >= 90) {
+		    System.out.println("A");
+		} else if (a >= 80) {
+		    System.out.println("B");
+		} else if (a >= 70) {
+		    System.out.println("C");
+		} else if (a >= 60) {
+		    System.out.println("D");
+		} else {
+		    System.out.println("E");
+		}
+	//質問２
+		   int total = 3340;
+		   int[] coin= {500, 100, 10, 5, 1};
+	       int[] coincounts = new int[coin.length];
+
+	        for (int i = 0; i < coin.length; i++) {
+	            coincounts[i] = total/ coin[i];
+	            total %= coin[i];
+	        }
+	        for (int i = 0; i < coin.length; i++) {
+	            System.out.println(coin[i] + "円: " + coincounts[i] + "枚");
+	        }
 	}
 }
