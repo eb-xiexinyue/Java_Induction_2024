5cd97f6b66f87187ddf0bc72260d107489b81c01
gfy78 <gfy78@threecats>
2023-12-06 00:00:26 +0900

    20231205 感觉很多课上都没讲过

--- src/co/jp/chapter04/JavaChapter04.java
+++ src/co/jp/chapter04/JavaChapter04.java
@@ -1,10 +1,25 @@
 package co.jp.chapter04;
 
+import java.util.Arrays;
+
 public class JavaChapter04 {
 
 	public static void main(String[] args) {
-
-
+		
+		int[] arr= {121,1231,454,4564,46545,32123,8948};
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				if(i!=j) {
+					if(arr[i]<arr[j]) {
+						int temp=arr[j];
+						arr[j]=arr[i];
+						arr[i]=temp;
+					}
+						
+				}
+			}
+System.out.println(Arrays.toString(arr));
+		}
 	}
 
 }
