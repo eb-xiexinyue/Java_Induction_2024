3d7a774fed73c37de019db00eb4ce689cefab273
Trafalgar <Trafalgar@192.168.3.5>
2023-12-11 11:05:58 +0900

    20231211 第5回（質問８は分からない）

--- src/co/jp/chapter05/JavaHomeWork05.java
+++ src/co/jp/chapter05/JavaHomeWork05.java
@@ -32,29 +32,29 @@ public class JavaHomeWork05 {
 //
 //
 //質問３︓配列 {1, 2, 6, 7, 9, 6, 2, 1} の最大値、最小値、和、平均値を求める。
-		int[] array = new int[] {1, 2, 6, 7, 9, 6, 2, 1};
-			int max=array[0];
-			int min=array[0];
-			int sum = 0;
-     		double average;
-		
-		for (int i = 0; i < array.length; i++) {
-         if (array[i] > max) {
-				max = array[i];
-          }
-          if (array[i] < min) {
-           	min = array[i];
-         		}
-           sum += array[i];
-        }
-
-		average = (double) sum / array.length;
-		
-		System.out.println("最大値: " + max);
-        System.out.println("最小値: " + min);
-        System.out.println("和: " + sum);
-        System.out.println("平均値: " + average);
-		
+//		int[] array = new int[] {1, 2, 6, 7, 9, 6, 2, 1};
+//			int max=array[0];
+//			int min=array[0];
+//			int sum = 0;
+//     		double average;
+//		
+//		for (int i = 0; i < array.length; i++) {
+//         if (array[i] > max) {
+//				max = array[i];
+//          }
+//          if (array[i] < min) {
+//           	min = array[i];
+//         		}
+//           sum += array[i];
+//        }
+//
+//		average = (double) sum / array.length;
+//		
+//		System.out.println("最大値: " + max);
+//        System.out.println("最小値: " + min);
+//        System.out.println("和: " + sum);
+//        System.out.println("平均値: " + average);
+//		
 //回答：最大値: 9
 //		最小値: 1
 //		和: 34
@@ -62,85 +62,76 @@ public class JavaHomeWork05 {
 		
 		
 //質問４︓質問３の配列に重複ある要素を探す（要素の表示次数集計必要なし）。
-	
-		
+//     int[] array = new int[] {1, 2, 6, 7, 9, 6, 2, 1};
+//        Set<Integer> set = new HashSet<>();	
+//		 for (int num : array){
+//            if (!set.add(num)){ 
+//                System.out.println("重複ある要素：" + num);
+//            }
+//		 }
+//回答：重複ある要素：6
+//		重複ある要素：2
+//		重複ある要素：1
 //
-//
-//
+
 //質問５︓質問３の配列に値「７」のインデックスを求む。
-		int r_array = 7;
-
-    
-        int x = findIndex(array, r_array);
-
-        if (x != -1) {
-            System.out.println("値 " + r_array + " は配列のインデックス " + x + " にある。");
-        } else {
-            System.out.println("値 " + r_array + " は配列内に見つかない。");
-        }
-    }
-		public static int findIndex(int[] array, int r_array) {
-			for (int i = 0; i < array.length; i++) {
-				if (array[i] == r_array) {
-					return i; 
-            }
-        }
-					return -1; 
+//		int[] array = new int[] {1, 2, 6, 7, 9, 6, 2, 1};
+//		int r_array = 7;
+//
+//    
+//        int x = findIndex(array, r_array);
+//
+//        if (x != -1) {
+//            System.out.println("値 " + r_array + " は配列のインデックス " + x + " にある。");
+//        } else {
+//            System.out.println("値 " + r_array + " は配列内に見つかない。");
+//        	}
+//		}
+//		public static int findIndex(int[] array, int r_array) {
+//			for (int i = 0; i < array.length; i++) {
+//				if (array[i] == r_array) {
+//					return i; 
+//            }
+//        }
+//					return -1; 
 //回答：値 7 は配列のインデックス 3 にある。
 
 
 //質問６︓任意行列（２次元配列）の積を求める。
+//		int[][] array = new int{　};//配列を入力してください
+					
+//		int product = 1;
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[i].length; j++) {
+//                product *= array[i][j];
+//            }
+//        }
+// 		System.out.println("積: " + product);
+//		｝
+//	｝
 //
-//
-//
-//
-//
-//質問７︓配列を昇順でソートする（java.util等メソッド利用禁止）。
-//
-//
-//
-//
-//
-//
+
+//質問７︓配列を昇順でソートする（java.util等メソッド利用禁止）。									
+//		int[] array = new int[] {1, 2, 6, 7, 9, 6, 2, 1};	
+//		 for (int i=0;i<array.length;i++) {
+//            for (int j=0;j<array.length;j++) {
+//                if(i!=j) {
+//					if(array[i]<array[j]) {
+//						int temp = array[j];
+//						array[j] = array[i];
+//						array[i] = temp;
+//					}
+//                }
+//            }
+//        }
+// 
+//        for (int num : array) {
+//            System.out.print(num + " ");//1 1 2 2 6 6 7 9 
+//        }
+//       
+
 //質問８︓LV10のパスカルの三角形を出力してください。		
-//		
-//		
-//		
-//		
-//		
-//		
-//		
-//		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+//不懂  
+ 
 	}
 }
