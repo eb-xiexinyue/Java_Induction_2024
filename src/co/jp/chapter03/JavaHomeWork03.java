b3e435c7867ab72438572cb1b4eb901da56dc2b5
81803 <81803@MyComputer.elecom>
2023-12-12 21:18:03 +0900

    2022 12 12

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -4,7 +4,53 @@ public class JavaHomeWork03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//質問１︓成績ランキング判断を作成する「90以上︓A, 80-90︓B, 70-80︓C, 60-70︓ D, 60以下︓E」
+		/*int a = 56;
+		System.out.println("成績ランキング判断。入力値=" + a);
+		// 以下コードを完成してください。
+		if(a>90) {
+		 System.out.println("A");
+		} else if(80<a<=90) {
+		 System.out.println("B");
+		} else if(70<a<=80) {
+		 System.out.println("C");
+		} else if(60<=a<=70) {
+		 System.out.println("D");
+		} else {
+		 System.out.println("E");
+		}
+		*/
 
 
+		//質問２︓⽇本のコインを１円、５円、１０円、１００円、５００円があります。３３４０円のコイン数をもとめください。
+		
+		/* public class KazuCounter {
+		    public static void main(String[] args) {
+		        int totalAmount = 3340;
+
+		        // 500円のコイン
+		        int coin500 = totalAmount / 500;
+		        totalAmount %= 500;
+
+		        // 100円のコイン
+		        int coin100 = totalAmount / 100;
+		        totalAmount %= 100;
+
+		        // 10円のコイン
+		        int coin10 = totalAmount / 10;
+
+		        // 1円のコイン
+		        int coin1 = totalAmount % 10;
+
+		        // 総コインの枚数
+		        int totalCoins = coin500 + coin100 + coin10 + coin1;
+
+		        System.out.println("500円: " + coin500 + "枚");
+		        System.out.println("100円: " + coin100 + "枚");
+		        System.out.println("10円: " + coin10 + "枚");
+		        System.out.println("1円: " + coin1 + "枚");
+		        System.out.println("総コインの枚数: " + totalCoins + "枚");
+		    }
+		}*/
 	}
 }
