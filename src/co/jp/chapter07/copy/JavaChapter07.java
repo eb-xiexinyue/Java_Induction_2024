4135373b5acabea7136f4d85783c5a3db4481abb
kertiticaca <kertiticaca@localhost>
2023-12-29 00:06:02 +0900

    20231218

---
+++ src/co/jp/chapter07/copy/JavaChapter07.java
@@ -0,0 +1,11 @@
+package co.jp.chapter07.copy;
+
+public class JavaChapter07 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("hello world");
+
+	}
+}
