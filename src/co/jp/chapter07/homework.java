f14995a3a5c86d1f3f8084427b25966449cd8aa9
81803 <81803@MyComputer.elecom>
2024-01-29 21:59:07 +0900

    2024 1 29

---
+++ src/co/jp/chapter07/homework.java
@@ -0,0 +1,58 @@
+package co.jp.chapter07;
+
+public class homework {
+
+	public static void main(String[] args) {
+		/*问题1
+		 *ペット・クラスにはsetAgeという名前のメソッドが定義されていないため、このコードはコンパイル・エラーを引き起こす。 
+		 *Kicker.javaファイルでは、setAgeメソッドを呼び出そうとしていますが、Petクラスにはこのメソッドの定義はありません。
+		 
+		 
+		 问题2
+		 public class Math {
+         public static int max(int[] input) {
+        if (input == null || input.length == 0) {
+            throw new IllegalArgumentException("Input array is null or empty.");
+        }
+
+        int max = input[0];
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] > max) {
+                max = input[i];
+            }
+        }
+        return max;
+    }
+
+    public static int min(int[] input) {
+        if (input == null || input.length == 0) {
+            throw new IllegalArgumentException("Input array is null or empty.");
+        }
+
+        int min = input[0];
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] < min) {
+                min = input[i];
+            }
+        }
+        return min;
+    }
+
+    public static double average(int[] input) {
+        if (input == null || input.length == 0) {
+            throw new IllegalArgumentException("Input array is null or empty.");
+        }
+
+        int sum = 0;
+        for (int value : input) {
+            sum += value;
+        }
+        return (double) sum / input.length;
+    }
+		 */
+		
+		
+
+	}
+
+}
---
+++ src/co/jp/chapter09/Homework.java
@@ -0,0 +1,20 @@
+package co.jp.chapter09;
+
+public class Homework {
+	/*问题1
+	このコード行ではbtnボタンのアクション・リスナーとしてラムダ式を使っている。
+	ラムダ式の構文は（引数）→式で、引数はボタンのクリック・イベントに関する情報を表すActionEvent eである。
+	そして、矢印 -> の後の部分は、実行されるコードのブロック、つまりボタンがクリックされたときに実行されるアクションです。 
+	ここでは、"ボタンクリックしました "というメッセージをコンソールに表示します。*/
+	
+	/*问题2
+	 * new Thread() {
+    @Override
+    public void run() {
+        System.out.println("thread running...");
+    }
+}.start();
+	 */
+	
+
+}
